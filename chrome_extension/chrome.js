25eb19b915f58844864e8538fc016025d3199156
YongHun Chang <yh12.chang@gmail.com>
2023-11-19 06:15:20 -0600

    Added basic files and setup for chrome extension

---
+++ chrome_extension/chrome.js
@@ -0,0 +1,8 @@
+console.log("chrome extension go")
+
+// let movie = document.getElementsByClassName("PZPZlf ssJ7i B5dxMB")
+// let movie = document.querySelectorAll("[class*='yuRUbf']")
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log(document.querySelectorAll('a'))
+})
